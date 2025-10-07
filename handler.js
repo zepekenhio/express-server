219be00e02fef0bbbbadcb7815d3ce0bbb69fdbd
Moukhtar Ben Ali <moukhtar.benali@gmail.com>
2025-10-07 15:59:13 +0100

    New update

--- handler.js
+++ handler.js
@@ -1,12 +1,16 @@
 
-const blogs = {results : []}
+const blogs = {results : [], nextId: 1}
 
 module.exports.getHandler = (req,res)=>{
     res.json(blogs.results)
 }
 module.exports.postHandler = (req,res)=>{
-    blogs.results.push(req.body)
-    res.send('Item added')
+    const newItem = {
+        id: blogs.nextId++,
+        ...req.body
+    }
+    blogs.results.push(newItem)
+    res.json({message: 'Item added', item: newItem})
 }
 module.exports.errorHandler = (req,res)=>{
     res.status(404).send('Not found')
@@ -16,8 +20,8 @@ module.exports.mainHandler = (req,res)=>{
 }
 
 module.exports.getItemHandler = (req,res)=>{
-    const id = parseInt(req.params.id)
-    const item = blogs.results[id]
+    const id = req.params.id
+    const item = blogs.results.find(item => item.id === id)
     if(item){
         res.json(item)
     } else {
@@ -26,20 +30,25 @@ module.exports.getItemHandler = (req,res)=>{
 }
 
 module.exports.deleteHandler = (req,res)=>{
-    const id = parseInt(req.params.id)
-    if(id >= 0 && id < blogs.results.length){
-        blogs.results.splice(id, 1)
-        res.send('Item deleted')
+    const id = req.params.id
+    const itemIndex = blogs.results.findIndex(item => item.id === id)
+    if(itemIndex !== -1){
+        const deletedItem = blogs.results.splice(itemIndex, 1)[0]
+        res.json({message: 'Item deleted', item: deletedItem})
     } else {
         res.status(404).send('Item not found')
     }
 }
 
 module.exports.putHandler = (req,res)=>{
-    const id = parseInt(req.params.id)
-    if(id >= 0 && id < blogs.results.length){
-        blogs.results[id] = req.body
-        res.send('Item updated')
+    const id = req.params.id
+    const itemIndex = blogs.results.findIndex(item => item.id === id)
+    if(itemIndex !== -1){
+        blogs.results[itemIndex] = {
+            id: id,  // Keep the original ID
+            ...req.body
+        }
+        res.json({message: 'Item updated', item: blogs.results[itemIndex]})
     } else {
         res.status(404).send('Item not found')
     }
