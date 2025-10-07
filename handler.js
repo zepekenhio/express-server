60f46abf5745fdb1c330f1dbc510a8b8fdcd46e0
imen-mekni <113175332+imen-mekni@users.noreply.github.com>
2025-10-07 12:45:53 +0100

    initial commit

---
+++ handler.js
@@ -0,0 +1,15 @@
+
+const blogs = {results : []}
+
+module.exports.getHandler = (req,res)=>{
+
+}
+module.exports.postHandler = (req,res)=>{
+
+}
+module.exports.errorHandler = (req,res)=>{
+
+}
+module.exports.mainHandler = (req,res)=>{
+      return res.send('hello world')
+}
