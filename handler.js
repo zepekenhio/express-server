4d24882ca4db3b987a2f24614f30315af431b643
Moukhtar Ben Ali <moukhtar.benali@gmail.com>
2025-10-07 15:16:19 +0100

    Exercice de realiser par Moukhtar Ben Ali

--- handler.js
+++ handler.js
@@ -2,14 +2,45 @@
 const blogs = {results : []}
 
 module.exports.getHandler = (req,res)=>{
-
+    res.json(blogs.results)
 }
 module.exports.postHandler = (req,res)=>{
-
+    blogs.results.push(req.body)
+    res.send('Item added')
 }
 module.exports.errorHandler = (req,res)=>{
-
+    res.status(404).send('Not found')
 }
 module.exports.mainHandler = (req,res)=>{
       return res.send('hello world')
+}
+
+module.exports.getItemHandler = (req,res)=>{
+    const id = parseInt(req.params.id)
+    const item = blogs.results[id]
+    if(item){
+        res.json(item)
+    } else {
+        res.status(404).send('Item not found')
+    }
+}
+
+module.exports.deleteHandler = (req,res)=>{
+    const id = parseInt(req.params.id)
+    if(id >= 0 && id < blogs.results.length){
+        blogs.results.splice(id, 1)
+        res.send('Item deleted')
+    } else {
+        res.status(404).send('Item not found')
+    }
+}
+
+module.exports.putHandler = (req,res)=>{
+    const id = parseInt(req.params.id)
+    if(id >= 0 && id < blogs.results.length){
+        blogs.results[id] = req.body
+        res.send('Item updated')
+    } else {
+        res.status(404).send('Item not found')
+    }
 }
--- server.js
+++ server.js
@@ -4,14 +4,21 @@ const handler = require('./handler')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 
 
 app.get('/', handler.mainHandler)
 
-app.get()
-app.post()
+app.get('/items', handler.getHandler)
+app.post('/items', handler.postHandler)
 
-app.get('*') //error handler
+app.get('/items/:id', handler.getItemHandler)
+app.delete('/items/:id', handler.deleteHandler)
+app.put('/items/:id', handler.putHandler)
+
+//app.get('*', handler.errorHandler) //error handler
+app.use(handler.errorHandler) //error handler
 
 app.listen(port, () => {
   console.log(`Express app listening on port http://localhost:${port}`)
